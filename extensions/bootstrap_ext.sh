744d8f81cb7ee95689c18a3eabda73024cdadc3a
X-McKay <aldmckay@gmail.com>
2022-03-20 14:45:30 -0400

    Added tailscale to ext

--- extensions/bootstrap_ext.sh
+++ extensions/bootstrap_ext.sh
@@ -1,11 +1,21 @@
 
 
 ubuntu_install_extras() {
-wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-sudo apt-get install ./exa_0.9.0-4_amd64.deb
+    # exa terminal toy
+    wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
+    sudo apt-get install ./exa_0.9.0-4_amd64.deb
 }
+    
 
 
 darwin_install_extras() {
     brew install exa
+}
+
+
+
+setup_tailscale() {
+    # Tailscale for VPN
+    curl -fsSL https://tailscale.com/install.sh | sh
+    sudo tailscale up
 }
